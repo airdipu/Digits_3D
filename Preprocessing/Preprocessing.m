7840cfc1db50c023361b910c1a9ad51e3b79c0a2
airdipu <airdipu@gmail.com>
2021-12-04 20:24:36 +0200

    added preprocessing file

--- Preprocessing/Preprocessing.m
+++ Preprocessing/Preprocessing.m
@@ -4,41 +4,42 @@ data_dir = dir('dat');
 N = size(data_dir, 1);
 
 
-%load('stroke_0_0001.mat')
-%X= pos(:,1);
-%Y=pos(:,2);
-%fig = plot(X,Y);
-%saveas(fig,'number.jpg')
-%x=[X,Y]';
-%Bin = imbinarize(x);
+%load('stroke_0_0001.mat')              % Load the data
+%X= pos(:,1);                           % Identify the x-axis data
+%Y=pos(:,2);                            % Identify the y-axis data
+%fig = plot(X,Y);                       % Plot the image of x-y axis
+%saveas(fig,'number.png')               % Save the image in a formate
+%figure
+%imshowpair(I,BW,'montage')
+%x=[X,Y]';                              % Creating an array
+%Bin = imbinarize(x);                   % Creating logical data
 
 
 
 
 % Resizing the images
-l = 20;
-w = 20;
-im_resize = l * w;
-data = zeros(N-2, im_resize);
+l = 25;
+w = 25;
+%im_resize = l * w;
+data = zeros(N-2, (l*w));               % Using l*w size 
 
 for i = 3 : N
         im = load(data_dir(i).name);
-        posit = im.pos;
-        x = posit(:, 1);                  % Position
-        y = posit(:, 2);                  % Position
-        figure('visible', 'off');       %to see images, comment this line
+        %posit = im.pos;
+        x = im.pos(:, 1);                  % Position
+        y = im.pos(:, 2);                  % Position
+        %figure('visible', 'off');       %to see images, comment this line
         axis auto
-        f1 = plot(x, y);                %plotting the image using 2 dims
-        set(gca, 'Visible', 'off');
-        saveas(f1, 'img', 'png');       %making an image object
-        img = imread('img.png');
+        fig = plot(x, y);                %plotting the image using 2 dims
+        %set(gca, 'Visible', 'off');
+        saveas(fig, 'fig', 'png');       %making an image object
+        img = imread('fig.png');
         img = imresize(img,[l w]);      %resizing all images to one size 
-        %img = rgb2gray(img);           %1)grayscale preprocessing
-        img = im2bw(img, 0.99);         %2)binarization
-        imshow(img);
-        trans_data = reshape(img, 1, []);       %transform array of pixels into line
-        data(i - 2,:) = trans_data;             %saving each image into data table
+        img = im2bw(img, 0.99); %imbinarize(img);            % Creating logical data
+        %imshow(img);
+        array_line = reshape(img, 1, []);       % Array of pixels to line
+        %data(i - 2,:) = array_line;             % Saving into data table
         disp(i);
 end
-% data = data / 255;                     %scaling for grayscale image
+
 save('data.mat','data');
