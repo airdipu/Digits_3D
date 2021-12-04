6b56b1eb30c9484557130bc3ab4b051a45647ac0
airdipu <airdipu@gmail.com>
2021-12-04 17:12:58 +0200

    added preprocessing file

--- Preprocessing/Preprocessing.m
+++ Preprocessing/Preprocessing.m
@@ -1,13 +1,25 @@
 % Adding the path of data
-addpath('training_data');
-data_dir = dir('training_data');
+addpath('dat');
+data_dir = dir('dat');
 N = size(data_dir, 1);
 
+
+%load('stroke_0_0001.mat')
+%X= pos(:,1);
+%Y=pos(:,2);
+%fig = plot(X,Y);
+%saveas(fig,'number.jpg')
+%x=[X,Y]';
+%Bin = imbinarize(x);
+
+
+
+
 % Resizing the images
-l = 50;
-w = 50;
+l = 20;
+w = 20;
 im_resize = l * w;
-data = zeros(N - 2, im_resize);
+data = zeros(N-2, im_resize);
 
 for i = 3 : N
         im = load(data_dir(i).name);
@@ -19,7 +31,6 @@ for i = 3 : N
         f1 = plot(x, y);                %plotting the image using 2 dims
         set(gca, 'Visible', 'off');
         saveas(f1, 'img', 'png');       %making an image object
-        %print('img2','-dpng', '-noui')
         img = imread('img.png');
         img = imresize(img,[l w]);      %resizing all images to one size 
         %img = rgb2gray(img);           %1)grayscale preprocessing
