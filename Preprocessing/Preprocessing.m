b151b5d505bc2cb6a0e9465323c37137f2f16e0b
airdipu <airdipu@gmail.com>
2021-12-04 14:50:36 +0200

    added preprocessing file

--- Preprocessing/Preprocessing.m
+++ Preprocessing/Preprocessing.m
@@ -1,6 +1,3 @@
-clear all;
-clc;
-
 % Adding the path of data
 addpath('training_data');
 data_dir = dir('training_data');
@@ -13,14 +10,13 @@ im_resize = l * w;
 data = zeros(N - 2, im_resize);
 
 for i = 3 : N
-        I = load(data_dir(i).name);
-        position = I.pos;
-
-        X = position(:, 1);                  %first dimension
-        Y = position(:, 2);                  %second dimension
+        im = load(data_dir(i).name);
+        posit = im.pos;
+        x = posit(:, 1);                  % Position
+        y = posit(:, 2);                  % Position
         figure('visible', 'off');       %to see images, comment this line
         axis auto
-        f1 = plot(X, Y);                %plotting the image using 2 dims
+        f1 = plot(x, y);                %plotting the image using 2 dims
         set(gca, 'Visible', 'off');
         saveas(f1, 'img', 'png');       %making an image object
         %print('img2','-dpng', '-noui')
@@ -29,8 +25,8 @@ for i = 3 : N
         %img = rgb2gray(img);           %1)grayscale preprocessing
         img = im2bw(img, 0.99);         %2)binarization
         imshow(img);
-        el = reshape(img, 1, []);       %transform array of pixels into line
-        data(i - 2,:) = el;             %saving each image into data table
+        trans_data = reshape(img, 1, []);       %transform array of pixels into line
+        data(i - 2,:) = trans_data;             %saving each image into data table
         disp(i);
 end
 % data = data / 255;                     %scaling for grayscale image
