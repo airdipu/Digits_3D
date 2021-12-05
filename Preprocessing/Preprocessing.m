6bd1c9782958f9e55bda457d8e5a04126b5dae1d
airdipu <airdipu@gmail.com>
2021-12-05 13:59:23 +0200

    added preprocessing file

--- Preprocessing/Preprocessing.m
+++ Preprocessing/Preprocessing.m
@@ -10,9 +10,9 @@ N = size(data_dir, 1);
 %fig = plot(X,Y);                       % Plot the image of x-y axis
 %saveas(fig,'number.png')               % Save the image in a formate
 %figure
-%imshowpair(I,BW,'montage')
 %x=[X,Y]';                              % Creating an array
-%Bin = imbinarize(x);                   % Creating logical data
+%bin = imbinarize(x);                   % Creating logical data
+%imshowpair(fig, bin,'montage')
 
 
 
@@ -20,24 +20,18 @@ N = size(data_dir, 1);
 % Resizing the images
 l = 25;
 w = 25;
-%im_resize = l * w;
 data = zeros(N-2, (l*w));               % Using l*w size 
 
 for i = 3 : N
         im = load(data_dir(i).name);
-        %posit = im.pos;
         x = im.pos(:, 1);                  % Position
         y = im.pos(:, 2);                  % Position
-        %figure('visible', 'off');       %to see images, comment this line
-        axis auto
         fig = plot(x, y);                %plotting the image using 2 dims
-        %set(gca, 'Visible', 'off');
         saveas(fig, 'fig', 'png');       %making an image object
-        img = imread('fig.png');
-        img = imresize(img,[l w]);      %resizing all images to one size 
-        img = im2bw(img, 0.99); %imbinarize(img);            % Creating logical data
-        %imshow(img);
-        array_line = reshape(img, 1, []);       % Array of pixels to line
+        im_read = imread('fig.png');
+        im_size = imresize(im_read,[l w]);      %resizing all images to one size 
+        im_logical = im2bw(im_size, 0.99); %imbinarize(img);            % Creating logical data
+        array_line = reshape(im_logical, 1, []);       % Array of pixels to line
         %data(i - 2,:) = array_line;             % Saving into data table
         disp(i);
 end
