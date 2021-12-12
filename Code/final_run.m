449c3cd4deea28d909f5d43839bb3bc3f8273e4a
airdipu <airdipu@gmail.com>
2021-12-12 15:39:34 +0200

    final_run added

--- Code/final_run.m
+++ Code/final_run.m
@@ -5,25 +5,25 @@ close all
 % Data loading
 input_data = zeros(100, 3, 1000);          % According to input data to
                                            % Createing 100 points for x,y,z
-size_Of_Data = [100 100 100 100 100 100 100 100 100 100];
-
-% Class vectors
-data_Num = [repmat(0,size_Of_Data(1), 1); repmat(1, size_Of_Data(2), ...
-    1); repmat(2, size_Of_Data(3), 1); repmat(3, size_Of_Data(4), 1); ...
-    repmat(4, size_Of_Data(5), 1); repmat(5, size_Of_Data(6), 1); ...
-    repmat(6, size_Of_Data(7), 1); repmat(7, size_Of_Data(8), 1); ...
-    repmat(8, size_Of_Data(9), 1); repmat(9, size_Of_Data(10), 1)];
-
-
-size_Of_Data_Cum = cumsum(size_Of_Data);
-size_Of_Data_Cum = [0 size_Of_Data_Cum];   % Zero for the loop (later)
 
 stroke_Size = [];                          % Rows
+
+size_Data = [100 100 100 100 100 100 100 100 100 100];
+
+% Class vectors
+data_Num = [repmat(0, 100, 1); repmat(1, 100, 1); ...
+    repmat(2, 100, 1); repmat(3, 100, 1); repmat(4, 100, 1); ...
+    repmat(5, 100, 1); repmat(6, 100, 1); repmat(7, 100, 1); ...
+    repmat(8, 100, 1); repmat(9, 100, 1)];
+
+
+size_Data_Cum = cumsum(size_Data);
+size_Data_Cum = [0 size_Data_Cum];   % Zero for the loop (later)
 
 
 % Loop for extracting data
 for i = 1:10                               % For 0 to 9
-        for j = 1:size_Of_Data(i)
+        for j = 1:size_Data(i)
             if j < 10
             C = strcat('stroke_', num2str(i-1), '_000', ...
                 num2str(j), '.mat');
@@ -39,11 +39,11 @@ for i = 1:10                               % For 0 to 9
             
             for k = 1:size(pos, 2)
             pos(:, k) = (pos(:, k) - min(pos(:, k)))/(max(pos(:, k)) ...
-                -min(pos(:, k)));          % Normalization using min-max
+                - min(pos(:, k)));          % Normalization using min-max
             
             end
             
-            input_data(1:size(pos, 1), :, size_Of_Data_Cum(i) + j) = pos;
+            input_data(1:size(pos, 1), :, size_Data_Cum(i) + j) = pos;
             
         end
 end
@@ -66,23 +66,30 @@ n_Rot_X_Data_Set = zeros(100, 3, size(input_data, 3)*size(deg_Of_Rot, 2));
 stroke_Size_X_Rot = repmat(stroke_Size, 1, size(deg_Of_Rot, 2));
 
 for i = 1:size(deg_Of_Rot, 2)              % Size of rotation vector
-for j = 1:size(input_data, 3)              % Size of data (3rd dim)
-angle = deg2rad(deg_Of_Rot(i));            % Changing degree to radian
+    
+    for j = 1:size(input_data, 3)          % Size of data (3rd dim)
+        
+        angle = deg2rad(deg_Of_Rot(i));            % Changing degree to radian
 
 
-% Rotation around x axis (Using DIIP)
-R_3D_Rot = [cos(angle) 0 sin(angle); 0 1 0; -sin(angle) 0 ...
-    cos(angle)];
+        % Rotation around x axis (Using DIIP)
+        % Transformation matrix
+        R_3D_Rotation = [cos(angle) 0 sin(angle); 0 1 0; -sin(angle) 0 ...
+            cos(angle)];
 
-data_Set_Of_Rot = input_data(1:stroke_Size(j), :, j)*R_3D_Rot;
-n_Rot_X_Data_Set(1:size(data_Set_Of_Rot, 1), :, (i - 1)* ...
-    size(input_data, 3) + j) = bsxfun(@minus, data_Set_Of_Rot, ...
-    min(data_Set_Of_Rot, [], 1))./repmat((max(data_Set_Of_Rot, ...
-    [], 1) - min(data_Set_Of_Rot, [], 1)), size(data_Set_Of_Rot, 1), 1);
-% Each step of j a new matrix adapted from the initial matrix
-% in 3rd dimension of the data_rotation matrix
+        data_Set_Of_Rot = input_data(1:stroke_Size(j), :, j)*R_3D_Rotation;
+        
+        n_Rot_X_Data_Set(1:size(data_Set_Of_Rot, 1), :, (i - 1)* ...
+            size(input_data, 3) + j) = bsxfun(@minus, data_Set_Of_Rot, ...
+            min(data_Set_Of_Rot, [], 1))./repmat((max(data_Set_Of_Rot, ...
+            [], 1) - min(data_Set_Of_Rot, [], 1)), ...
+            size(data_Set_Of_Rot, 1), 1);
 
-end
+        % Each step of j a new matrix adapted from the initial matrix
+        % in 3rd dimension of the data_rotation matrix
+
+    end
+
 end
 
 Num_data_x_rot = repmat(data_Num, size(deg_Of_Rot, 2), 1);
@@ -96,33 +103,36 @@ save('Num_data_x_rot.mat', 'Num_data_x_rot')
 
 % Processing
 
-% Angle of rotation
-deg_Of_Rot = [5, 10, 15, 20, 25, 30, 35, 40, 45];
-
 n_Rot_Y_Data_Set = zeros(100, 3, size(input_data, 3)* ...
     size(deg_Of_Rot, 2));
 
 stroke_Size_Y_Rot = repmat(stroke_Size, 1, size(deg_Of_Rot, 2));
 
 for i = 1:size(deg_Of_Rot, 2)              % Size of rotation vector
-for j = 1:size(input_data, 3)              % Size of data (3rd dim)
     
-angle = deg2rad(deg_Of_Rot(i));            % Changing degree to radian
+    for j = 1:size(input_data, 3)          % Size of data (3rd dim)
+    
+        angle = deg2rad(deg_Of_Rot(i));    % Changing degree to radian
 
-% Rotation around y axis (Using DIIP)
-R_3D_Rot = [cos(angle) 0 -sin(angle); 0 1 0; sin(angle) 0 ...
-    cos(angle)];
+        % Rotation around y axis (Using DIIP)
+        % Transformation matrix
+        R_3D_Rotation = [cos(angle) 0 -sin(angle); 0 1 0; sin(angle) 0 ...
+            cos(angle)];
 
-data_Set_Of_Rot = input_data(1:stroke_Size(j), :, j)*R_3D_Rot;
+        data_Set_Of_Rot = input_data(1:stroke_Size(j), :, j)* ...
+            R_3D_Rotation;
 
-n_Rot_Y_Data_Set(1:size(data_Set_Of_Rot, 1), :, (i - 1)* ...
-    size(input_data, 3) + j) = bsxfun(@minus, data_Set_Of_Rot, ...
-    min(data_Set_Of_Rot, [], 1))./repmat((max(data_Set_Of_Rot, ...
-    [], 1) - min(data_Set_Of_Rot, [], 1)), size(data_Set_Of_Rot, 1), 1);
-% Each step of j a new matrix adapted from the initial matrix
-% in 3rd dimension of the data_rotation matrix
+        n_Rot_Y_Data_Set(1:size(data_Set_Of_Rot, 1), :, (i - 1)* ...
+            size(input_data, 3) + j) = bsxfun(@minus, data_Set_Of_Rot, ...
+            min(data_Set_Of_Rot, [], 1))./repmat((max(data_Set_Of_Rot, ...
+            [], 1) - min(data_Set_Of_Rot, [], 1)), ...
+            size(data_Set_Of_Rot, 1), 1);
 
-end
+        % Each step of j a new matrix adapted from the initial matrix
+        % in 3rd dimension of the data_rotation matrix
+
+    end
+    
 end
 
 Num_data_y_rot = repmat(data_Num, size(deg_Of_Rot, 2), 1);
@@ -131,36 +141,41 @@ save('n_Rot_Y_Data_Set.mat', 'n_Rot_Y_Data_Set')
 save('stroke_Size_Y_Rot.mat', 'stroke_Size_Y_Rot') 
 save('Num_data_y_rot.mat', 'Num_data_y_rot')
 
+
 % Rotation on z-axis
 
 % Processing
-
-% Angle of rotation
-deg_Of_Rot = [5, 10, 15, 20, 25, 30, 35, 40, 45];
 
 n_Rot_Z_Data_Set = zeros(100, 3, size(input_data, 3)*size(deg_Of_Rot, 2));
 
 stroke_Size_Z_Rot = repmat(stroke_Size, 1, size(deg_Of_Rot, 2));
 
 for i = 1:size(deg_Of_Rot, 2)              % Size of rotation vector
-for j = 1:size(input_data, 3)              % Size of data (3rd dim)
+    
+    for j = 1:size(input_data, 3)          % Size of data (3rd dim)
+        
 
-angle = deg2rad(deg_Of_Rot(i));            % Changing degree to radian
+        angle = deg2rad(deg_Of_Rot(i));    % Changing degree to radian
 
-% Rotation around z axis (Using DIIP)
-R_3D_Rot =[cos(angle) sin(angle) 0; -sin(angle) cos(angle) 0; ...
-    0 0 1];
+        % Rotation around z axis (Using DIIP)
+        % Transformation matrix
+        R_3D_Rotation =[cos(angle) sin(angle) 0; -sin(angle) cos(angle) ...
+            0; 0 0 1];
 
-data_Set_Of_Rot = input_data(1:stroke_Size(j), :, j)*R_3D_Rot;
+        data_Set_Of_Rot = input_data(1:stroke_Size(j), :, j)* ...
+            R_3D_Rotation;
 
-n_Rot_Z_Data_Set(1:size(data_Set_Of_Rot, 1), :, (i - 1)* ...
-    size(input_data, 3) + j) = bsxfun(@minus, data_Set_Of_Rot, ...
-    min(data_Set_Of_Rot, [], 1))./repmat((max(data_Set_Of_Rot, [], 1) ...
-    - min(data_Set_Of_Rot, [], 1)), size(data_Set_Of_Rot, 1), 1);
-% Each step of j a new matrix adapted from the initial matrix
-% in 3rd dimension of the data_rotation matrix
+        n_Rot_Z_Data_Set(1:size(data_Set_Of_Rot, 1), :, (i - 1)* ...
+            size(input_data, 3) + j) = bsxfun(@minus, data_Set_Of_Rot, ...
+            min(data_Set_Of_Rot, [], 1))./repmat((max(data_Set_Of_Rot, ...
+            [], 1) - min(data_Set_Of_Rot, [], 1)), ...
+            size(data_Set_Of_Rot, 1), 1);
 
-end
+        % Each step of j a new matrix adapted from the initial matrix
+        % in 3rd dimension of the data_rotation matrix
+
+    end
+    
 end
 
 Num_data_z_rot = repmat(data_Num, size(deg_Of_Rot, 2), 1);
@@ -173,6 +188,10 @@ save('Num_data_z_rot.mat', 'Num_data_z_rot')
 % Combining rotated x, y, z axis data
 size_data = size(new_Data, 3) + size(n_Rot_X_Data_Set, 3) + ...
     size(n_Rot_Y_Data_Set, 3) + size(n_Rot_Z_Data_Set, 3);
+
+stroke_Size_Com = [stroke_Size stroke_Size_X_Rot stroke_Size_Y_Rot ...
+    stroke_Size_Z_Rot];
+
 
 N_data_Com = zeros(222, 3, size_data);
 N_data_Com(:, :, 1: size(new_Data, 3)) = new_Data;
@@ -187,8 +206,6 @@ N_data_Com(:, :, size(new_Data, 3) + size(n_Rot_X_Data_Set, 3) + ...
 N_data_Com(:, :, size(new_Data, 3) + size(n_Rot_X_Data_Set, 3) + ...
     size(n_Rot_Y_Data_Set, 3) + 1:size_data) = n_Rot_Z_Data_Set;
 
-stroke_Size_Com = [stroke_Size stroke_Size_X_Rot stroke_Size_Y_Rot ...
-    stroke_Size_Z_Rot];
 
 % Combining classes
 Num_data_Com = [data_Num; Num_data_x_rot; Num_data_y_rot; ...
@@ -201,12 +218,12 @@ save('Num_data_Com.mat', 'Num_data_Com')
 
 % Adaptation approach
 slope_val = zeros(4, size(stroke_Size_Com, 2));
-w = 0.20;                                  % Initial data
+u = 0.20;                                  % Initial value
 
 for m = 1:size(stroke_Size_Com, 2)
     
-    slope_val(1:2, m) = [N_data_Com(round(stroke_Size_Com(m)*w), 2, m) ...
-        - N_data_Com(1, 2, m); N_data_Com(round(stroke_Size_Com(m)*w), ...
+    slope_val(1:2, m) = [N_data_Com(round(stroke_Size_Com(m)*u), 2, m) ...
+        - N_data_Com(1, 2, m); N_data_Com(round(stroke_Size_Com(m)*u), ...
         1, m) - N_data_Com(1, 1, m)];
     
     slope_val(3, m) = sum(diff(N_data_Com(1:stroke_Size_Com(m), 1, m)));
@@ -244,7 +261,7 @@ for k = 1:size(N_data_Com, 3)
     for y = 0:step_size:1
         n = 0;                             % For projection on y
         m = 0;                             % For projection on x
-        for q = 1:stroke_Size_Com(k) - 1   % -1 since also use of q + 1
+        for q = 1:stroke_Size_Com(k) - 1   % -1 for q + 1
              if N_data_Com(q, 1, k) < N_data_Com(q + 1, 1, k)
                 % If the first point is smaller than the second point, the
                 % solver can find a slope
@@ -259,7 +276,7 @@ for k = 1:size(N_data_Com, 3)
                 y1 = N_data_Com(q + 1, 2, k);
             end
             l = (y2 - y1)/(x2 - x1);       % For y-slope
-            ll = y1 - (l*x1);              % Intersect for y
+            ll = y1 - (l*x1);              % Intercept for y
             x_val = (y - ll)/l;
             % Close and open interval
             if x_val >= min([x1, x2]) && x_val < max([x1, x2])
@@ -276,6 +293,7 @@ for k = 1:size(N_data_Com, 3)
             
         end
         
+        % Updating n and m
         project_y(round((y + step_size)/step_size)) = n;
         project_x(round((y + step_size)/step_size)) = m;
     
@@ -295,38 +313,42 @@ load('data_Num.mat')                       % Class data
 
 % Adaptation of train class 0-9 --> 1-10
 
-train_rate = 0.7;                         % Size of train dataset
+train_rate = 0.8;                         % Size of train dataset
 
 x_Train = randsample(size(new_Data, 3), round(train_rate* ...
-    size(new_Data, 3)));                   % Random index of train data
+    size(new_Data, 3)));                  % Random index of train data
 
 % Random index of test data
 x_Test = setdiff(1:size(new_Data, 3), x_Train)';
 
 % Train and Test data
-train_pro = train_profile(:, x_Train);
+train_data = train_profile(:, x_Train);
+test_data = train_profile(:, x_Test);
+
 train_classes = data_Num(x_Train, 1);
-test_pro = train_profile(:, x_Test);
 test_classes = data_Num(x_Test, 1);
 
 % Fitting knn
-classes = knn(train_classes, train_pro, test_pro, 7);
+classes = knn(train_classes, train_data, test_data, 7);
 
 % Measurements
 correct_classes = sum(classes == test_classes)/ ...
     size(test_classes, 1);
 
-avg_correct_class = mean(correct_classes);
+avarage_correct_class = mean(correct_classes);
 
-accuracy = ((classes - (correct_classes))/classes)*100
+accuracy = ((classes - correct_classes)/classes)*100
 
 
 % Testing of a single digit data
 load('stroke_0_0099.mat')
+
+% To see the digit
 x1 = pos(:, 1);
 y1 = pos(:, 2);
+
 plot(x1, y1)
-Clas = digit_classify(pos)
+C = digit_classify(pos)
 
 
 
@@ -346,3 +368,4 @@ Clas = digit_classify(pos)
 % https://towardsdatascience.com/machine-learning-basics-with-the-k-nearest-neighbors-algorithm-6a6e71d01761
 % https://en.wikipedia.org/wiki/K-nearest_neighbors_algorithm
 % https://www.javatpoint.com/k-nearest-neighbor-algorithm-for-machine-learning
+% https://codereview.stackexchange.com/questions/46027/k-nearest-neighbours-in-matlab
